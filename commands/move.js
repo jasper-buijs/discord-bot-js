7b434af30bae8fd42f91eb997b5aa4b5b2ff9816
Jasper Buijs <jabuijs@icloud.com>
2023-03-26 16:13:39 +0200

    Invite Slash Command Added invite command to create temporary invites. Fixed a deferal issue in the move command. Bumped version number to 3-1-0.

--- commands/move.js
+++ commands/move.js
@@ -3,9 +3,10 @@ const { SlashCommandBuilder, PermissionFlagsBits, ChannelType } = require('disco
 module.exports = {
     data: new SlashCommandBuilder().setName("move").setDescription("Move everyone from your voice channel to another.").setDefaultMemberPermissions(PermissionFlagsBits.Administrator).addChannelOption(op => op.setName("channel").setDescription("Channel to move to.").setRequired(true).addChannelTypes(ChannelType.GuildVoice)),
     async execute(client, interaction) {
+        await interaction.deferReply({ ephemeral: true });
         if (!interaction.member.voice.channelId) {
             console.log("> ERROR with MOVE: user not in voice channel");
-            return await interaction.reply({ content: "You are not in a voice channel.", ephemeral: true });
+            return await interaction.editReply({ content: "You are not in a voice channel.", ephemeral: true });
         }
         const voiceChannelFrom = interaction.member.voice.channel;
         const voiceChannelTo = interaction.options.getChannel("channel");
@@ -13,5 +14,6 @@ module.exports = {
         await voiceChannelFrom.members.forEach(member => {
             member.voice.setChannel(voiceChannelTo);
         });
+        await interaction.editReply({ content: "Users successfully moved", ephemeral: true });
     }
 }
