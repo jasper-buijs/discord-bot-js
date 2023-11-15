c0de9d8bb6e5e1323d7c702a9f1e1ba34c5c13e3
WoutWenseleers <woutwenseleers@gmail.com>
2023-11-15 23:37:07 +0100

    indentation

--- deploy.js
+++ deploy.js
@@ -5,19 +5,19 @@ const commands = new Array();
 const commandsFolder = fs.readdirSync("./commands");
 
 for (const commandFile of commandsFolder) {
-    const command = require("./commands/" + commandFile);
-    if ("structure" in command && "execute" in command) {
-        commands.push(command.structure.toJSON());
-    }
+  const command = require("./commands/" + commandFile);
+  if ("structure" in command && "execute" in command) {
+    commands.push(command.structure.toJSON());
+  }
 }
 
 const rest = new REST().setToken(Bun.env.TOKEN);
 
 (async () => {
-    try {
-        await rest.put(Routes.applicationGuildCommands(Bun.env.CLIENTID, Bun.env.GUILDID), { body: commands });
-        console.log("commands successfully registered")
-    } catch (error) {
-        console.error(error);
-    }
+  try {
+    await rest.put(Routes.applicationGuildCommands(Bun.env.CLIENTID, Bun.env.GUILDID), { body: commands });
+    console.log("commands successfully registered")
+  } catch (error) {
+    console.error(error);
+  }
 })(); 
--- index.js
+++ index.js
@@ -6,23 +6,23 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.commands = new Collection();
 const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
 for (const commandFile of commandFiles) {
-    const command = require("./commands/" + commandFile);
-    client.commands.set(command.structure.name, command);
+  const command = require("./commands/" + commandFile);
+  client.commands.set(command.structure.name, command);
 }
 
 client.on("interactionCreate", async interaction => {
-    if (interaction.type == InteractionType.ApplicationCommand) {
-        const command = client.commands.get(interaction.commandName);
-        if (!command) return;
-        try {
-            await command.execute(client, interaction);
-        } catch (error) {
-            console.error(error);
-            await interaction.reply({ content: "There was an error :(" }).catch();
-            await interaction.editReply({ content: "There was an error :(" }).catch();
-            await interaction.followUp({ content: "There was an error :(" }).catch();
-        }
+  if (interaction.type == InteractionType.ApplicationCommand) {
+    const command = client.commands.get(interaction.commandName);
+    if (!command) return;
+    try {
+      await command.execute(client, interaction);
+    } catch (error) {
+      console.error(error);
+      await interaction.reply({ content: "There was an error :(" }).catch();
+      await interaction.editReply({ content: "There was an error :(" }).catch();
+      await interaction.followUp({ content: "There was an error :(" }).catch();
     }
+  }
 });
 
 client.login(Bun.env.TOKEN);
