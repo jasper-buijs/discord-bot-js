6bc57f8f5373138f39287a1a7c04fe213cf5e2b1
Jasper Buijs <62234929+jasper-buijs@users.noreply.github.com>
2024-07-16 13:21:30 +0200

    Better error handling in interaction-create.ts event file Changed the way errors are handled in interaction-create.ts, so not every error leads to program termination. Commit towards issue #4

--- events/client/interaction-create.ts
+++ events/client/interaction-create.ts
@@ -9,14 +9,24 @@ export const once = false;
 export async function execute(client: ClientProps, ...args: any[]) {
   const interaction: Interaction = args[0];
   if (interaction.type == InteractionType.ApplicationCommand) {
-    if (!client.commands) return;
+    if (!client.commands) throw new Error("A slash command was used, but no commands are registered in client.commands");
     const command = client.commands.get(interaction.commandName);
-    if (!command) return;
+    if (!command) throw new Error("A slash command was used, but it was not registered in client.commands");
     try { await command.execute(client, interaction); } catch (error) {
       console.error(error);
-      await interaction.reply({ content: "There was an error :(" }).catch();
-      await interaction.editReply({ content: "There was an error :(" }).catch();
-      await interaction.followUp({ content: "There was an error :(" }).catch()
+      try { await interaction.reply({ content: "There was an error executing this command." }) } catch (error) {
+        console.error("Failed to reply with error message!");
+        console.error(error);
+        try { await interaction.editReply({ content: "There was an error executing this command." }) } catch (error) {
+          console.error("Failed to editReply with error message!");
+          console.error(error);
+          try { await interaction.followUp({ content: "There was an error executing this command." }) } catch (error) {
+            console.error("Failed to followUp with error message, throwing!");
+            console.error(error);
+            throw new Error("ApplicationCommand failed to execute. An error message could not be sent through reply, editReply or followUp.");
+          }
+        }
+      }
     }
   }
 }
